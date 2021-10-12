327953c423444698518d957efc7ee9e587ad628e
HunSpino <tamasleventelaszlo99@gmail.com>
2021-10-12 17:32:21 +0200

    adat kiíródik(x és checkbox még nem működik)

--- main.js
+++ main.js
@@ -4,6 +4,18 @@ function general(){
     let li = document.createElement("li");
     let input = document.createElement("input");
     input.type = "checkbox";
+    var text = document.createTextNode("\t" + adat);
+    let close = document.createElement("button");
+    var x = document.createTextNode("x");
+    close.className = "delete";
+
+    close.appendChild(x);
+
+    li.appendChild(input);
+    li.appendChild(text);
+    li.appendChild(close);
+
+    document.getElementById('lista').appendChild(li);
 
 
 }
