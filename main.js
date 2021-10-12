7b329b97fb6834fe3daa122d2cc3e37ec441c757
HunSpino <tamasleventelaszlo99@gmail.com>
2021-10-12 17:14:23 +0200

    háttérszín adás(szemkímélő),listadó cucc pozícionálása,js-ben folytatódik

--- main.js
+++ main.js
@@ -1,4 +1,13 @@
 
 function general(){
-    
+    let adat = document.getElementById('addBox').value;
+    let li = document.createElement("li");
+    let input = document.createElement("input");
+    input.type = "checkbox";
+
+
 }
+function init() {
+    document.getElementById('addGomb').addEventListener("click", general)
+}
+document.addEventListener("DOMContentLoaded",init);
